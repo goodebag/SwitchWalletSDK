4b0dfc5f2b02c455f647a7fcc8818eee3a4fc739
goodebag <egbule.goodluck@gmail.com>
2023-03-03 19:25:29 +0100

    Unit testing

---
+++ SwitchWalletSDK.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SwitchWalletSDK.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
